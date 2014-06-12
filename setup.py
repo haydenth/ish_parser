7447f57a6d1f9e9634f7ef5fa22169a1ad2a2f44
Tom Hayden <thayden@grubhub.com>
2014-06-12 09:54:00 -0500

    adding my stuff into setup

--- setup.py
+++ setup.py
@@ -2,5 +2,8 @@ from distutils.core import setup
 
 setup(name='ish_parser',
       version='0.0.1',
+      author_email='thayden@gmail.com',
+      author='thayden',
+      url='https://github.com/haydenth/ish_parser',
       package_dir={'': 'src'},
       py_modules=['ish_parser'])
