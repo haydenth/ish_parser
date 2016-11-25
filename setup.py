e603cda1d86b2ecb601b591a4f3a1300d3ae5d88
Tom Hayden <thayden@gmail.com>
2016-11-25 13:47:08 -0600

    version 0.11

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='ish_parser',
-      version='0.0.10',
+      version='0.0.11',
       author_email='thayden@gmail.com',
       description='Parser for NOAA ISH (integrated surface hourly) reports',
       author='thayden',
