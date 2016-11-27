ddce223766f9e37532396bf56813614590fd9d10
Tom Hayden <thayden@gmail.com>
2016-11-27 14:03:43 -0600

    doing release 0.14 and fixing busted include statement

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 
 setup(name='ish_parser',
-      version='0.0.13',
+      version='0.0.14',
       author_email='thayden@gmail.com',
       description='Parser for NOAA ISH (integrated surface hourly) reports',
       author='thayden',
       url='https://github.com/haydenth/ish_parser',
       packages = ['ish_parser'],
       package_dir={'ish_parser': 'src'},
-      py_modules=['ish_parser', 'ish_report', 'Temperature', 'Observation', 'Units', 'Components', 'Distance', 'Speed', 'Direction', 'Pressure', 'ReportType', 'Humidity'])
+      py_modules=['ish_parser', 'ish_report'])
