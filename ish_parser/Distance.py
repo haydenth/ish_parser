18bd79fb1abb91c485bfedf3e235a7c17794905a
mrmucox <mrmucox@gmail.com>
2019-11-04 09:50:15 -0500

    Update Distance.py

--- ish_parser/Distance.py
+++ ish_parser/Distance.py
@@ -6,16 +6,19 @@ class Distance(Observation):
   between farenheight and celsius, yay '''
   MILLIMETERS = 1
   METERS = 2
-  INCHES = 2
+  INCHES = 3
   MISSING = [999, 99999, 999999]
-  INCH_CONVERSION_FACTOR = 0.393701
+  # Inches per cm
+  INCH_CONVERSION_FACTOR = 1/2.54
 
   def get_inches(self):
     ''' convert the measurement to inches '''
     if self._obs_value in self.MISSING:
       return 'MISSING'
     if self._obs_units == self.MILLIMETERS:
-      return round(self.INCH_CONVERSION_FACTOR * self._obs_value, 4)
+      return round(self.INCH_CONVERSION_FACTOR * 10 * self._obs_value, 4)
+    if self._obs_units == self.METERS:
+      return round(self.INCH_CONVERSION_FACTOR / 100 * self._obs_value, 4)
 
   def __str__(self):
     if self._obs_value in self.MISSING:
