95e6bacf704569cab43971bb66e0854c7225e4c5
mrmucox <mrmucox@gmail.com>
2019-11-05 14:05:47 -0500

    Fix get_inches() and get_miles() conversions
    Fix get_inches() and get_miles() conversions. conversion from mm or m to cm had the wrong operator.

--- ish_parser/Distance.py
+++ ish_parser/Distance.py
@@ -16,18 +16,18 @@ class Distance(Observation):
     if self._obs_value in self.MISSING:
       return 'MISSING'
     if self._obs_units == self.MILLIMETERS:
-      return round(self._obs_value * 10 * self.INCH_CONVERSION_FACTOR, 4)
+      return round(self._obs_value / 10 * self.INCH_CONVERSION_FACTOR, 4)
     if self._obs_units == self.METERS:
-      return round(self._obs_value / 100 * self.INCH_CONVERSION_FACTOR, 4)
+      return round(self._obs_value * 100 * self.INCH_CONVERSION_FACTOR, 4)
 
   def get_miles(self):
     ''' convert the measurement to inches '''
     if self._obs_value in self.MISSING:
       return 'MISSING'
     if self._obs_units == self.MILLIMETERS:
-      return round(self._obs_value * 10 * self.INCH_CONVERSION_FACTOR / 12 / 5280, 4)
+      return round(self._obs_value / 10 * self.INCH_CONVERSION_FACTOR / 12 / 5280, 4)
     if self._obs_units == self.METERS:
-      return round(self._obs_value / 100 * self.INCH_CONVERSION_FACTOR / 12 / 5280, 4)
+      return round(self._obs_value * 100 * self.INCH_CONVERSION_FACTOR / 12 / 5280, 4)
 
   def __str__(self):
     if self._obs_value in self.MISSING:
