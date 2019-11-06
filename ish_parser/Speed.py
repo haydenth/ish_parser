82f60f67dc6568eb4e4fb0b41924e91e9486b6dd
mrmucox <mrmucox@gmail.com>
2019-11-06 09:03:31 -0500

    Fix MPH_PER_MPS

--- ish_parser/Speed.py
+++ ish_parser/Speed.py
@@ -6,7 +6,7 @@ class Speed(Observation):
   METERSPERSECOND = 1
   MILESPERHOUR = 2
   SPEED_SCALE = 10
-  mph_per_mps = 100 / 2.54 / 12 / 5280 * 60 * 60
+  MPH_PER_MPS = 100 / 2.54 / 12 / 5280 * 60 * 60
   MISSING = [999, 9999, 999.9]
 
   def get_MilesPerHour(self):
@@ -14,7 +14,7 @@ class Speed(Observation):
     if self._obs_value in self.MISSING:
       return None
     if self._obs_units == self.METERSPERSECOND:
-      return round(self._obs_value * mph_per_mps, 4)
+      return round(self._obs_value * self.MPH_PER_MPS, 4)
 
   def __str__(self):
     if self._obs_value in self.MISSING:
