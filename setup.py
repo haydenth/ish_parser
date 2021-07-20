7e32366055fc3976907f56a48f20b4654186c669
Tom Hayden <thayden@gmail.com>
2021-07-20 09:51:03 -0500

    updating setup.py to v23

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(name='ish_parser',
-      version='0.0.22',
-      author_email='thayden@gmail.com',
-      description='Parser for NOAA ISH (integrated surface hourly) reports',
-      author='thayden',
-      url='https://github.com/haydenth/ish_parser',
-      packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-      py_modules=['ish_parser', 'ish_report'])
+  version='0.0.23',
+  author_email='thayden@gmail.com',
+  description='Parser for NOAA ISH (integrated surface hourly) reports',
+  author='thayden',
+  url='https://github.com/haydenth/ish_parser',
+  packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+  py_modules=['ish_parser', 'ish_report'])
